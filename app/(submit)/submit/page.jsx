b7ea583a74282bc473915029a02d6b6134c186db
WN95HL <130599937+WN95HL@users.noreply.github.com>
2024-04-13 10:08:57 -0400

    Create page.jsx

---
+++ app/(submit)/submit/page.jsx
@@ -0,0 +1,11 @@
+import GameInputForm from "@/components/client/GameInput/GameInputForm";
+
+function page() {
+  return (
+    <div className="h-96 flex justify-center items-center">
+      <GameInputForm />
+    </div>
+  );
+}
+
+export default page;

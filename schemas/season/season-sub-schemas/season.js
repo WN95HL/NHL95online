01b0409f5b64d634ad0e2d0e87063735a8eaf1fa
WN95HL <130599937+WN95HL@users.noreply.github.com>
2024-04-13 10:35:02 -0400

    Create season.js

---
+++ schemas/season/season-sub-schemas/season.js
@@ -0,0 +1,62 @@
+
+import { Schema } from "mongoose";
+import LeagueTableSchema from "./leagueTable";
+import LeaguesTeamsSchema from "./leagueTeamsSchema";
+import LeaguesConferencesSchema from "./conferences";
+
+const SeasonSchema = new Schema({
+  seasonNumber: String,
+  hasSeasonBegun: {
+    type: Boolean,
+    default: false,
+  },
+  hasSeasonEnded: {
+    type: Boolean,
+    default: false,
+  },
+  hasPlayoffsBegun: {
+    type: Boolean,
+    default: false
+  },
+  hasPlayoffsEnded: {
+    type: Boolean,
+    default:false
+  },
+  conferences: {
+    type: [LeaguesConferencesSchema],
+    default: [],
+  },
+  divisions: { type: [String], default: [] },
+  teams: [LeaguesTeamsSchema],
+  teamsDictCodes: {
+    type: Object,
+    default: {},
+  },
+  standings: [LeagueTableSchema],
+  divisionalGames: {
+    type: String,
+    default: "",
+  },
+  conferenceGames: {
+    type: String,
+    default: "",
+  },
+  otherConferenceGames: {
+    type: String,
+    default: "",
+  },
+  totalGamesToBePlayed: {
+    type: String,
+    default: "0",
+  },
+  startDate: {
+    type: Date,
+    default: null,
+  },
+  endDate: {
+    type: Date,
+    default: null,
+  },
+});
+
+export default SeasonSchema;

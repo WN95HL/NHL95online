027252c89a2f31acfa8d83c60914c15bc07c248a
WN95HL <130599937+WN95HL@users.noreply.github.com>
2024-04-13 10:15:05 -0400

    Create page.jsx

---
+++ app/(auth)/login/page.jsx
@@ -0,0 +1,9 @@
+import LogInForm from "@/components/client/Login/LoginForm";
+
+export default async function Page() {
+  return (
+    <div className="h-96 flex justify-center items-center text-slate-300">
+      <LogInForm />
+    </div>
+  );
+}
